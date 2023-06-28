3f64769f33049e05d2e5c1ca810ad5aad62cbc1a
clincaruto <clindrop@gmail.com>
2023-06-28 12:43:35 +0100

    latest fix

--- PosReversalNIBBS_API/Repositories/IRepository/ITokenHandlerRepo.cs
+++ PosReversalNIBBS_API/Repositories/IRepository/ITokenHandlerRepo.cs
@@ -1,9 +1,10 @@
 ﻿using PosReversalNIBBS_API.Models.Domain;
+using PosReversalNIBBS_API.Models.DTO;
 
 namespace PosReversalNIBBS_API.Repositories.IRepository
 {
     public interface ITokenHandlerRepo
     {
-        Task<string> CreateTokenAsync(User user);
+        Task<string> CreateTokenAsync(LoginRequest user);
     }
 }
